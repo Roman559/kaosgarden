b95b2a241ffba9ad627c3da81876244a7b0af3fb
Kaos Garden <chaosgardenmusic@gmail.com>
2026-05-28 17:15:52 -0400

    Added Google Analytics and Gooole arch Console

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,36 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Kaos Garden",
-  description: "Official Kaos Garden Website",
-};
+import Script from "next/script";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+
+      <body>
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-MHZ3ENFCJZ"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-MHZ3ENFCJZ');
+          `}
+        </Script>
+
+        {children}
+
+      </body>
+
     </html>
   );
 }
